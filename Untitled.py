6025badd4ad39a08f5a160b1039c9564cecebb95
barecheradouane2 <101993965+barecheradouane2@users.noreply.github.com>
2023-07-04 12:30:22 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# title of the network
+
+# This is introduction
+
+# DATA SCIENCE LANGUAGES ARE:
+#     1-python
+#     2-R
+#     3-SQL
+#     4-Julia
+#     5-Scala
+#     6-Java
+#     7-Julia
+#     8-Spark
+#     
+
+#  Data Science libraries are:
+#  1-NumPy
+#  2-Pandas
+#  3-Matplotlib
+#  4-scikit-learn
+#  5-TensorFlow
+#  6-Keras
+#  7-PyTorch
+#  8-SciPy
+#  9-Statsmodels
+#  10-Seaborn
+
+# | Language   | Libraries/Frameworks                       | Description                                              |
+# |------------|--------------------------------------------|----------------------------------------------------------|
+# | Github     |                                             |   
+# | R          | ggplot2, dplyr, caret, tidyr                | Designed for statistical computing and data analysis     |
+# | SQL        | PostgreSQL, MySQL, SQLite                   | Essential for working with relational databases          |
+# | Julia      | JuliaPro, Flux, MLJ                          | High-performance language for numerical computing        |
+# | Scala      | Apache Spark, MLlib                          | Used for distributed computing and big data processing   |
+# | Python     | NumPy, Pandas, Matplotlib, scikit-learn     |  Widely used for data manipulation, analysis, and MLdevelopment |
+# | SAS        | SAS Base, SAS/STAT, SAS Enterprise Miner     | Widely used for advanced analytics and data management   |
+# | Java       | Apache Hadoop, Weka                          | General-purpose language with big data and ML support    |
+# | Julia      | Flux, DataFrames.jl, MLJ                      | High-level language for technical computing              |
+# | Spark      | Apache Spark, MLlib                          | Distributed computing system for big data processing     |
+# 
+
+# arithmetic expression :
+# plus like 1+1=2;
+# mutlitple like 3*4=12;
+# 
+
+# In[5]:
+
+
+2*2+5
+
+
+# In[6]:
+
+
+minutes = 135 
+
+hours = minutes / 60
+
+print(f"{minutes} minutes is equal to {hours:.2f} hours.")
+
+
+#  list Objectives are:
+# 
+# 1. First list 
+# 2. Second list 
+# 3. Third list
+# 4. Fourth list
+
+# ## Author
+# 
+# Bareche Radouane
+
+# In[ ]:
+
+
+
+
